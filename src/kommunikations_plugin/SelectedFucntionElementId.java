8ff8fd72e7ef3962146f2c5b06fc8317818eb355
FaizanFaheem-RSE <faizanfaheem956@gmail.com>
2025-12-27 14:57:04 +0100

    Minor improvement and adding one comment for better readbility

--- src/kommunikations_plugin/SelectedFucntionElementId.java
+++ src/kommunikations_plugin/SelectedFucntionElementId.java
@@ -16,15 +16,15 @@ public class SelectedFucntionElementId {
 		PresentationElement selectedElement = project.getActiveDiagram().getSelected().get(0);
 
 		if (selectedElement != null && selectedElement.getElement() instanceof Activity) {
-			/* 
-			 * Checking if the model element in the specified SysML model is selected by the user. 
-			 * It is also checked that the selected model element is Activity/Function 
-			 * If both the described conditions are satisfied then the selected function ID will be returned to the SysMLModelDataSaver.java class. The 
-			 * associated SysML elements, their properties and associated relationships of the selected function or activity will be stored in the SysMLModelDataSaver.java class.
-			 */
+			
+                        // Here returing the ID of the selected Activity/Function 
+
 			funtionID = selectedElement.getElement().getID();
 		}
 		else {
+
+                        // This message will be displayed if a system element other than Activity/Function
+ is selected.
 			Application.getInstance().getGUILog().showMessage("Please select the Activity/Function from the specified SysML model") ;
 		}
 		return funtionID;
